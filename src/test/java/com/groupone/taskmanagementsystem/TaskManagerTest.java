29a8d1d13c0bca89e50145ff3bb44eead68595ac
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-03 04:17:09 -0500

    fix: Se arregla el error introducido de adrede
    4 errores que identifica PDM

--- src/test/java/com/groupone/taskmanagementsystem/TaskManagerTest.java
+++ src/test/java/com/groupone/taskmanagementsystem/TaskManagerTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-//@SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.LawOfDemeter"})
+@SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.LawOfDemeter"})
 class TaskManagerTest {
 
     private TaskManager taskManager;
