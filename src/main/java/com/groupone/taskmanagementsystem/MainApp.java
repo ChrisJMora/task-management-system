9cfbab026cf416daa524eaaddea5975df33c4639
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-03 01:45:08 -0500

    fix: Se hace inmutable a la clase MainApp.java

--- src/main/java/com/groupone/taskmanagementsystem/MainApp.java
+++ src/main/java/com/groupone/taskmanagementsystem/MainApp.java
@@ -3,10 +3,14 @@ package com.groupone.taskmanagementsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MainApp {
+public final class MainApp {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainApp.class);
 
-    public static void main(String[] args) {
+    private MainApp() {
+        throw new UnsupportedOperationException("Esta es una clase utilitaria y no puede ser instanciada");
+    }
+
+    public static void main(final String[] args) {
         LOGGER.info("Hola Mundo!");
     }
 }
