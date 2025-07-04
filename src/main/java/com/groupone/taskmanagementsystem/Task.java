54f7236204ee3edf3baae322d923245a9415c488
Jefferson Bladimir Ruiz Pico <146777678+JeffKun1999@users.noreply.github.com>
2025-07-04 10:53:04 -0500

    Corrección de la clase, no se guardó el contenido y se hizo commit vacía xD

---
+++ src/main/java/com/groupone/taskmanagementsystem/Task.java
@@ -0,0 +1,24 @@
+package com.groupone.taskmanagementsystem;
+
+public class Task {
+    private String nombre;
+
+    public Task(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "nombre='" + nombre + '\'' +
+                '}';
+    }
+}
