ac4ecd5951b62d13352bf51ca1679a11db8be4c9
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-05 12:53:52 -0500

    feat: Se crea la clase TaskRepository
    Define la lógica del CRUD

---
+++ src/main/java/com/groupone/taskmanagementsystem/TaskRepository.java
@@ -0,0 +1,51 @@
+package com.groupone.taskmanagementsystem;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SuppressWarnings({"PMD.LawOfDemeter"})
+public class TaskRepository implements ITaskRepository {
+
+    private final List<TaskItem> tasks;
+
+    public TaskRepository() {
+        this.tasks = new ArrayList<>();
+    }
+
+    @Override
+    public Optional<TaskItem> getTaskByIndex(final int index) {
+        Optional<TaskItem> existingTask;
+        if (index >= 0 && index < tasks.size()) {
+            existingTask = Optional.of(tasks.get(index));
+        } else {
+            existingTask = Optional.empty();
+        }
+        return existingTask;
+    }
+
+    @Override
+    public List<TaskItem> getAllTasks() {
+        return new ArrayList<>(tasks);
+    }
+
+    @Override
+    public boolean addTask(final String taskName) {
+        final int newTaskIndex = tasks.size();
+        final TaskItem newTask = new TaskItem(newTaskIndex, taskName);
+        return tasks.add(newTask);
+    }
+
+    @Override
+    public Optional<TaskItem> updateTaskByIndex(final int index, final String updatedName) {
+        return getTaskByIndex(index).map(task -> {
+            task.setName(updatedName);
+            return task;
+        });
+    }
+
+    @Override
+    public boolean removeTaskByIndex(final int index) {
+        return getTaskByIndex(index).map(tasks::remove).orElse(false);
+    }
+}
