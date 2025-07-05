a0a0f0ef9f117115d1059cd274b1829b42848a28
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-05 13:54:20 -0500

    feat: Se crea la interfaz ITaskService
    Define los métodos CRUD

---
+++ src/main/java/com/groupone/taskmanagementsystem/ITaskService.java
@@ -0,0 +1,17 @@
+package com.groupone.taskmanagementsystem;
+
+import java.util.Collection;
+
+public interface ITaskService {
+
+    Collection<TaskItem> getTaskCollection();
+
+    TaskItem getTaskByIndex(int taskIndex);
+
+    boolean addTask(TaskItem newTask);
+
+    TaskItem updateTaskByIndex(int taskIndex);
+
+    boolean removeTaskByIndex(int taskIndex);
+
+}
