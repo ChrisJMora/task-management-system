a4f4b323fa0dd1cef4dbc8269239570b4ead1f03
Christian Jácome <129555977+ChrisJMora@users.noreply.github.com>
2025-07-05 21:07:07 -0500

    feat: Se actualiza la clase MainApp para ejecutar la clase TaskConsoleInterface

--- src/main/java/com/groupone/taskmanagementsystem/MainApp.java
+++ src/main/java/com/groupone/taskmanagementsystem/MainApp.java
@@ -1,16 +1,11 @@
 package com.groupone.taskmanagementsystem;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+@SuppressWarnings({"PMD.AtLeastOneConstructor", "PMD.UseUtilityClass"})
 public final class MainApp {
-    private static final Logger LOGGER = LoggerFactory.getLogger(MainApp.class);
-
-    private MainApp() {
-        throw new UnsupportedOperationException("Esta es una clase utilitaria y no puede ser instanciada");
-    }
 
     public static void main(final String[] args) {
-        LOGGER.info("Hola Mundo!");
+        final TaskConsoleInterface consoleInterface = new TaskConsoleInterface();
+        consoleInterface.start();
     }
+
 }
